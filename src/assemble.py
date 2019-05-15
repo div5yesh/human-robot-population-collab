6636fa884843e0664877466d9051445972c620ff
Divyesh Chitroda <div5yesh@gmail.com>
2019-05-15 00:23:52 -0700

    add world, map, rviz file and custom move base

--- src/assemble.py
+++ src/assemble.py
@@ -14,18 +14,20 @@ class Assemble:
 
     def square(self):
         client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-        # client.wait_for_server()
+        client.wait_for_server()
 
         goal = MoveBaseGoal()
-        goal.target_pose.header.frame_id = "robot2_tf/base_link"
+        goal.target_pose.header.frame_id = "robot1_tf/base_footprint"
         goal.target_pose.header.stamp = rospy.Time.now()
-        goal.target_pose.pose.position.x = 2.0
+        goal.target_pose.pose.position.x = 1.0
+        goal.target_pose.pose.position.y = 1.0
         goal.target_pose.pose.orientation.w = 1.0
 
         client.send_goal(goal)
         client.wait_for_result(rospy.Duration.from_sec(5.0))
 
-        # print("Result:" + client.get_result())
+        print("Result:" + client.get_result())
+        print("State:" + client.get_state())
 
         if(client.get_state() == actionlib.SimpleGoalState.DONE):
             print("done")
